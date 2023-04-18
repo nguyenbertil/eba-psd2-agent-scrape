9c6625d165a1c7ec9e21590d5fb0c702fa404287
Bertil Nguyen <nguyenbertildev@gmail.com>
2023-04-18 20:15:46 +0200

    Added Ireland as option

--- scraper.py
+++ scraper.py
@@ -9,8 +9,9 @@ url = "https://euclid.eba.europa.eu/register/pir/search/agents"
 
 parent_url = "https://euclid.eba.europa.eu/register/api/entity/read/"
 
-s =requests.session()
+s = requests.session()
 # seems to be ENT_TYP_PAR_ENT+ENT_COD_PAR_ENT
+
 
 def get_search_key(properties):
     parent_entity_type = ""
@@ -25,24 +26,27 @@ def get_search_key(properties):
         raise RuntimeError("Could not find parent entity type or code")
     return f"{parent_entity_type}.{parent_entity_code}"
 
+
 def get_parent_details(agent):
     search_key = get_search_key(agent["Properties"])
-    response = s.get(f"{parent_url}{search_key}",params={"t": f"{int(time.time() * 1000)}"})
+    response = s.get(f"{parent_url}{search_key}", params={
+                     "t": f"{int(time.time() * 1000)}"})
     if response.status_code == 200:
         parent = response.json()
         parent = parent[0]['_payload']
-        renamed_props =[]
+        renamed_props = []
         for prop in parent["Properties"]:
             if len(prop) != 0:
                 key, value = list(prop.items())[0]
-                renamed_props.append({f"parent_{key}" : value})
+                renamed_props.append({f"parent_{key}": value})
         parent["Properties"] = renamed_props
-        
+
     else:
         raise RuntimeError(
             "Error, exiting early: {}".format(response.status_code))
-        
+
     return parent
+
 
 def format_payload_results(agent):
     updated_properties = {}
@@ -50,13 +54,15 @@ def format_payload_results(agent):
         updated_properties.update(prop)
     agent["Properties"] = updated_properties
     return agent
+
+
 def main(country_code):
-    bank_codes = {"BE": "BE_NBB", "ES" : "ES_BE"}
+    bank_codes = {"BE": "BE_NBB", "ES": "ES_BE", "IE": "IE_CBI"}
     if country_code not in bank_codes:
         raise ValueError("Invalid country code")
 
     json_filter = {"$and": [{"_payload.EntityType": "PSD_AG"}, {"_searchkeys": {"$elemMatch": {"T": "P", "K": "DER_CHI_ENT_AUT", "V": "YES"}}}, {
-        "_searchkeys": {"$elemMatch": {"T": "P", "K": "ENT_COU_RES", "V": "SE"}}}, {"_payload.CA_OwnerID": bank_codes[country_code] }]}
+        "_searchkeys": {"$elemMatch": {"T": "P", "K": "ENT_COU_RES", "V": "SE"}}}, {"_payload.CA_OwnerID": bank_codes[country_code]}]}
 
     response = s.post("https://euclid.eba.europa.eu/register/api/search/entities",
                       params={"t": f"{int(time.time() * 1000)}"}, json=json_filter)
@@ -70,18 +76,20 @@ def main(country_code):
             parent_details = get_parent_details(agent)
             agent = format_payload_results(agent)
             parent_details = format_payload_results(parent_details)
-            result.append({**agent["Properties"], **parent_details["Properties"]})
+            result.append({**agent["Properties"], **
+                          parent_details["Properties"]})
     else:
         # Handle error
         raise RuntimeError(
             "Error, exiting early: {}".format(response.status_code))
 
-    pd.DataFrame(result).to_csv('data.csv')    
+    pd.DataFrame(result).to_csv('data.csv')
 
 
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("--country", help="Country to scrape: options : ES or BE", required=True)
+    argument_parser.add_argument(
+        "--country", help="Country to scrape: options : [ES, BE, IE]", required=True)
     parsed_args = argument_parser.parse_args()
     country_code = parsed_args.country
     print("Starting scraper")
